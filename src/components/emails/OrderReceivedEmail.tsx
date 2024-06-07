6f7bb8c8343fbe449ca128a7d3eb727d247487dd
Deborah Ogunniran <deborahogunniran187@gmail.com>
2024-06-08 01:38:36 +0200

    changes

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -28,7 +28,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://example.com'
+      : 'https://casecobraweb.vercel.app/'
 
   return (
     <Html>
